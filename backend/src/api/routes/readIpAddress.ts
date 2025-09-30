3b8d2944bcd7d35cfffcbf8eb4980b90b2bc216d
Isaias Candido <isaiascandido92@outlook.com>
2025-09-30 16:37:12 -0300

    feat: adiciona a rota para registrar endereços de IP, e a rota para ler a quantidade de endreços de IP registrados

--- backend/src/api/routes/readIpAddress.ts
+++ backend/src/api/routes/readIpAddress.ts
@@ -2,17 +2,17 @@ import type { IncomingMessage, ServerResponse } from 'http'
 import db from '../../data/connection.js'
 
 export const readIpService = (_req: IncomingMessage, res: ServerResponse): void => {
-  // try {
-  //   const result = db.prepare(`
-  //     SELECT COUNT(DISTINCT ip_address) AS visitors_count FROM visitors_ip_address
-  //   `).get() as { visitors_count: number }
+  try {
+    const result = db.prepare(`
+      SELECT COUNT(DISTINCT ip_address) AS visitors_count FROM visitors_ip_address
+    `).get() as { visitors_count: number }
 
-  //   res.writeHead(200, { 'Content-Type': 'application/json' })
-  //   res.end(JSON.stringify({ count: result.visitors_count }))
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ count: result.visitors_count }))
 
-  // } catch (error) {
-  //   res.writeHead(500, { 'Content-Type': 'application/json' })
-  //   res.end(JSON.stringify({ error: 'Error counting IP addresses' }))
-  //   console.error(error)
-  // }
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ error: 'Error counting IP addresses' }))
+    console.error(error)
+  }
 }
